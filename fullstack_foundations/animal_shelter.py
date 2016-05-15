de9884b97ba0092ff4c545eef318ec432eb916c4
developertenzin <tenzinphuljung7@gmail.com>
2016-05-15 03:53:04 -0500

    Created animal_shelter.py puppyshelter and run.py.

---
+++ fullstack_foundations/animal_shelter.py
@@ -0,0 +1,35 @@
+import sys
+
+from sqlalchemy import Column, ForeignKey, Integer, String, Date
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class Shelter(Base):
+    __tablename__ = 'shelter'
+    name = Column(String(80), nullable=False)
+    address = Column(String(250), nullable=False)
+    city = Column(String(80), nullable=False)
+    state = Column(String(2), nullable=False)
+    zipCode = Column(Integer, nullable=False)
+    website = Column(String(250), nullable=False)
+    id = Column(Integer, primary_key=True)
+
+class Puppy(Base):
+    __tablename__ = 'puppy'
+    name = Column(String(80), nullable=False)
+    dateOfBirth = Column(Date)
+    gender = Column(String(6), nullable=False)
+    weight = Column(Integer)
+    id = Column(Integer, primary_key = True)
+    shelter_id = Column(Integer, ForeignKey('shelter.id'))
+
+    shelter = relationship(Shelter)
+######## insert at end of file #########
+
+
+engine = create_engine('sqlite:///puppyshelter.db')
+
+Base.metadata.create_all(engine)
---
+++ fullstack_foundations/run.py
@@ -0,0 +1,42 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import func
+
+#from database_setup import Restaurant, Base, MenuItem
+#engine = create_engine('sqlite:///restaurantmenu.db')
+from animal_shelter import Shelter, Puppy, Base
+engine = create_engine('sqlite:///puppyshelter.db')
+
+# Bind the engine to the metadata of the Base class so that the
+# declaratives can be accessed through a DBSession instance
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+# A DBSession() instance establishes all conversations with the database
+# and represents a "staging zone" for all the objects loaded into the
+# database session object. Any change made against the objects in the
+# session won't be persisted into the database until you call
+# session.commit(). If you're not happy about the changes, you can
+# revert all of them back to the
+# last commit by calling
+# session.rollback()
+session = DBSession()
+
+#print(session.query(MenuItem).all())
+#database = session.query(MenuItem).all()
+
+all_puppies = session.query(Puppy).all()
+
+acs_pups = session.query(Puppy).order_by(Puppy.name.asc())
+
+infants = session.query(Puppy).filter_by(Puppy.dateOfBirth > '2015-10-01').order_by(Puppy.dateOfBirth)
+
+
+
+
+
+#for item in database:
+#    print(item.name, item.id)
+
+
+
