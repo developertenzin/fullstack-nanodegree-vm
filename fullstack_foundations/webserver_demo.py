90322efd2e4036515fd996ca12f6c02c05dd0a00
developertenzin <tenzinphuljung7@gmail.com>
2016-05-22 15:45:03 -0500

    Implemented edit functionality and currently working on delete.

--- fullstack_foundations/webserver_demo.py
+++ fullstack_foundations/webserver_demo.py
@@ -16,6 +16,61 @@ session = DBSession()
 class webserverHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         try:
+            if self.path.endswith("/hello"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+
+                output = ""
+                output += "<html><body>"
+                output += "Hello!"
+                output += "<form method='POST' enctype='multipart/form-data' action='/hello'> \
+                <h2>What would you like me to say?</h2><input name='message' type='text'> <input type='submit' \
+                 value='Submit'> </form>"
+                output += "</body></html>"
+
+                self.wfile.write(bytes(output, "utf-8"))
+                print(output)
+                return
+
+            if self.path.endswith("/edit"):
+                restaurant_id = self.path.split("/")[1]
+                current_restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+                if current_restaurant:
+                    self.send_response(200)
+                    self.send_header('Content-type', 'text/html')
+                    self.end_headers()
+                    output = ""
+                    output += "<html><body>"
+                    output += "%s<br><br>" % current_restaurant.name
+                    output += "<form method='POST' enctype='multipart/form-data' action='restaurants/%s/edit'>" % restaurant_id
+                    output += '''<input style = 'font-size: 1em; border-radius: 4px; display: block; margin-bottom: 10px;
+                                 box-shadow: none;' name='message' enctype='multipart/form-data' type='text'
+                                 placeholder='new name'>'''
+                    output += "<input type='submit' value='Rename'><br>"
+                    output += "</form"
+                    output += "</body></html>"
+                    self.wfile.write(bytes(output, 'utf-8'))
+                    return
+
+            if self.path.endswith("/delete"):
+                restaurant_id = self.path.split("/")[1]
+                current_restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+                if current_restaurant:
+                    self.send_response(200)
+                    self.send_header('Content-type', 'text/html')
+                    self.end_headers()
+                    output = ""
+                    output += "<html><body>"
+                    output += "<form method='POST' enctype='multipart/form-data' action='restaurants/%s/delete'>" % restaurant_id
+                    output += "Are you sure you want to delete %s?" % current_restaurant.name
+                    output += "<input type = 'hidden' name = 'country' value = 'Norway'>"
+                    output += "<input type='submit' value = 'delete'>"
+                    output += "</form>"
+                    output += "</body></html>"
+                    self.wfile.write(bytes(output, 'utf-8'))
+                    return
+
             if self.path.endswith("/restaurants"):
                 self.send_response(200)
                 self.send_header('Content-type', 'text/html')
@@ -24,12 +79,15 @@ class webserverHandler(BaseHTTPRequestHandler):
                 output = ""
                 output += "<html><body>"
                 restaurants = session.query(Restaurant.name).all()
+                count = 1
                 for restaurant in restaurants:
                     output += "<p> %s <p>" % restaurant
-                    output += "<a href='#'>Edit</a><br>"
-                    output += "<a href='#'>Delete</a>"
+                    output += "<a href='%i/edit'>Edit</a><br>" % count
+                    output += "<a href='%i/delete'>Delete</a>" % count
                     output += "<div style='margin-bottom: 50px'></div>"
+                    count += 1
 
+                output += "<a href='/restaurants/new'> Create A New Restaurant </a>"
                 output += "</body></html>"
 
                 self.wfile.write(bytes(output, "utf-8"))
@@ -52,30 +110,64 @@ class webserverHandler(BaseHTTPRequestHandler):
                 print(output)
                 return
 
+            if self.path.endswith("/restaurants/new"):
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+
+                output = ""
+                output += "<html><body>"
+                output += "<h1> Make A New Restaurant </h1>"
+                output += "<form method='POST' enctype='multipart/form-data' action='/restaurants/new'>"
+                output += "<input style = 'font-size: 1em; border-radius: 4px; display: block; margin-bottom: 10px; box-shadow: none;' name='message' enctype='multipart/form-data' type='text' placeholder='restaurant name'>"
+                output += "<input type='submit' value='Submit'>"
+                output += "</form>"
+                output += "</body></html>"
+                self.wfile.write(bytes(output, "utf-8"))
+                print(output)
+                return
+
         except IOError:
             self.send_error(404, "File Not Found %s" % self.path)
 
     def do_POST(self):
         try:
-            self.send_response(301)
-            self.end_headers()
-            length = self.headers['content-length']
-            post_data = self.rfile.read(int(length))
-            # print("***********", post_data, "***********", type(post_data))
-            # post_data is bytes!!!!!!!!!!!
-            # resource-link: https://reecon.wordpress.com/2014/04/02/simple-http-server-for-testing-get-and-post-requests-python/
-            post_data = str(post_data,'utf-8')
-            print(type(post_data), "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
-            content = post_data.split("\r\n\r\n")[1].split("\r\n")[0]
-            output = ""
-            output += "<html><body>"
-            output += "<h2> Okay, how about this: </h2>"
-            output += "<h1> %s </h1>" % content
-            output += "<form method='POST' enctype='multipart/form-data' action='/hello'> \
-                <h2>What would you like me to say?</h2><input name='message' type='text'> <input type='submit' \
-                 value='Submit'> </form>"
-            output += "</body></html>"
-            self.wfile.write(bytes(output, "utf-8"))
+            if self.path.endswith("/edit"):
+                self.send_response(301)
+                self.send_header("Content-type", "text/html")
+                self.send_header("Location", "/restaurants")
+                self.end_headers()
+                length = self.headers['content-length']
+                post_data = self.rfile.read(int(length))
+                post_data = str(post_data,'utf-8')
+                content = post_data.split("\n")[3]
+                restaurant_id = self.path.split("/")[1]
+                current_restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
+
+                current_restaurant.name = content
+                session.add(current_restaurant)
+                session.commit()
+                return
+
+            if self.path.endswith("/delete"):
+                self.send_response(301)
+                self.send_header('Content-type', 'text/html')
+                self.send_header('Location', '/restaurants')
+                self.end_headers()
+
+            if self.path.endswith("/restaurants/new"):
+                    self.send_response(301)
+                    self.send_header("Content-type", "text/html")
+                    self.send_header("Location", "/restaurants")
+                    self.end_headers()
+                    length = self.headers['content-length']
+                    post_data = self.rfile.read(int(length))
+                    post_data = str(post_data,'utf-8')
+                    content = post_data.split("\n")[3]
+                    restaurant = Restaurant(name = content)
+                    session.add(restaurant)
+                    session.commit()
+
         except:
             pass
             #python 2.7code
